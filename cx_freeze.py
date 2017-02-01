a94dbe8f91232a755baf95855050bb5fe7207a5c
dhb52 <dhb52@126.com>
2017-02-02 01:06:05 +0800

    style PEP8 cx_freeze script

--- cx_freeze.py
+++ cx_freeze.py
@@ -3,11 +3,12 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-	"packages": ["os"], 
-	"excludes": ["tkinter"],
-	"include_files": [#r'D:\Python27\Lib\site-packages\shapely\DLLs\geos_c.dll',
-                      'app.ico',
-                      ],
+    "packages": ["os"],
+    "excludes": ["tkinter"],
+    "include_files": [
+        # r'D:\Python27\Lib\site-packages\shapely\DLLs\geos_c.dll',
+        'app.ico',
+    ],
 }
 
 # GUI applications require a different base on Windows (the default is for a
@@ -16,8 +17,8 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "KML tools",
-        version = "0.1",
-        description = "KML tools",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("kmltools.pyw", base=base, icon='app.ico')])
+setup(name="KML tools",
+      version="0.1",
+      description="KML tools",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("kmltools.pyw", base=base, icon='app.ico')])
